9c9edadd4780c6befe03700844b6eb341c71c100
FuleiNataliya <fulei.nataliya@gmail.com>
2018-12-17 21:29:45 +0200

    patternState

---
+++ src/refactoring_guru/state/example/states/Demo.java
@@ -0,0 +1,15 @@
+package refactoring_guru.state.example.states;
+
+import utils.Player;
+import utils.UI;
+
+/**
+ * Demo class. Everything comes together here.
+ */
+public class Demo {
+    public static void main(String[] args) {
+        Player player = new Player();
+        UI ui = new UI(player);
+        ui.init();
+    }
+}
